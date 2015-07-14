8f6422c202e4094d134045b11223039ed143044e
jasonandrews271192@gmail.com <jasonandrews271192@gmail.com>
2015-07-14 03:33:24 +0100

    Added more basic GUI elements.

--- GuiFrame.java
+++ GuiFrame.java
@@ -17,12 +17,22 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
 
 public class GuiFrame extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel mainMenuPane;
+	private JPanel configurePane;
+	private JPanel loginPane;
+	
 	private JTable table;
 	private JTable table_1;
+	private JTextField urlTextField;
+	private JTextField userTextField;
+	private JPasswordField pwPasswordField;
 
 	/**
 	 * Launch the application.
@@ -58,30 +68,43 @@ public class GuiFrame extends JFrame {
 		setBounds(100, 100, 687, 490);
 		setResizable(false);
 		
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(null);
-		setContentPane(contentPane);
+		mainMenuPane = new JPanel();
+		mainMenuPane.setBackground(Color.WHITE);
+		mainMenuPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		mainMenuPane.setLayout(null);		
 		
-		JButton configureBtn = new JButton("Configure");
-		
+		/*
+		JButton configureBtn = new JButton("Configure");		
 		configureBtn.setToolTipText("Configure the database settings.");
 		configureBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				displayConfiguration();
 				//When the Configure button is clicked/pressed.
 			}
 		});
-		configureBtn.setBounds(268, 128, 122, 33);
-		contentPane.add(configureBtn);
+		configureBtn.setBounds(268, 172, 122, 33);
+		mainMenuPane.add(configureBtn);
 		
 		JButton loginBtn = new JButton("Login");
-		loginBtn.setBounds(268, 172, 122, 33);
-		contentPane.add(loginBtn);
+		loginBtn.setBounds(268, 128, 122, 33);
+		mainMenuPane.add(loginBtn);
 		
 		JButton exitBtn = new JButton("Exit");
 		exitBtn.setBounds(268, 216, 122, 33);
-		contentPane.add(exitBtn);
+		mainMenuPane.add(exitBtn);
+		*/
+		//Creating the elements of the Configuration pane.
+		configurePane = new JPanel();
+		configurePane.setBackground(Color.WHITE);
+		configurePane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		configurePane.setLayout(null);
+		configurePane.setBounds(0,0,0,0);
 		
+		urlTextField = new JTextField();
+		urlTextField.setBounds(242, 142, 226, 20);
+		mainMenuPane.add(urlTextField);
+		urlTextField.setColumns(10);
+		//configurePane
 		/*
 		 
 		// Creates a table, adds a scroll to it and fills in the table with the dummy info. 
@@ -124,5 +147,60 @@ public class GuiFrame extends JFrame {
 		table_1.setBorder(new LineBorder(new Color(0, 0, 0)));
 		
 		*/
+		setContentPane(mainMenuPane);
+		
+		JLabel urlLbl = new JLabel("URL:");
+		urlLbl.setHorizontalAlignment(SwingConstants.RIGHT);
+		urlLbl.setBounds(173, 145, 61, 14);
+		mainMenuPane.add(urlLbl);
+		
+		userTextField = new JTextField();
+		userTextField.setColumns(10);
+		userTextField.setBounds(242, 173, 226, 20);
+		mainMenuPane.add(userTextField);
+		
+		JLabel userLbl = new JLabel("User:");
+		userLbl.setHorizontalAlignment(SwingConstants.RIGHT);
+		userLbl.setBounds(173, 176, 61, 14);
+		mainMenuPane.add(userLbl);
+		
+		JLabel pwLbl = new JLabel("Password:");
+		pwLbl.setHorizontalAlignment(SwingConstants.RIGHT);
+		pwLbl.setBounds(151, 207, 83, 14);
+		mainMenuPane.add(pwLbl);
+		
+		pwPasswordField = new JPasswordField();
+		pwPasswordField.setBounds(242, 204, 226, 20);
+		mainMenuPane.add(pwPasswordField);
+		
+		JButton connectBtn = new JButton("Connect");
+		connectBtn.setBounds(282, 235, 73, 23);
+		mainMenuPane.add(connectBtn);
+		
+		JButton resetBtn = new JButton("Reset");
+		resetBtn.setBounds(365, 235, 73, 23);
+		mainMenuPane.add(resetBtn);
+		
+	
+		
+	}
+	
+	void displayMainMenu() {
+		setContentPane(mainMenuPane);
+		configurePane.setVisible(false);
+		loginPane.setVisible(false);
+	}
+	
+
+	void displayConfiguration() {
+		mainMenuPane.setVisible(false);
+		loginPane.setVisible(false);
+		setContentPane(configurePane);
+	}
+	
+	void displayLogin() {
+		mainMenuPane.setVisible(false);
+		loginPane.setVisible(false);
+		setContentPane(loginPane);
 	}
 }
